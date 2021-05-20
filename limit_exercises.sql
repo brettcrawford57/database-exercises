b3031851f178adf01e92b0a50f3c81f4b0991c24
Brett Crawford <brett.crawford57@gmail.com>
2021-05-20 14:46:47 -0500

    finished limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,22 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'Z%'
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT salary
+FROM salaries
+ORDER BY emp_no ASC
+LIMIT 5;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
